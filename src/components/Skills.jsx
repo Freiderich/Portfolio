49e9f93a3eeb4284c7865d500985c52e0963c50f
Freiderich S. Peralta <149556109+Freiderich@users.noreply.github.com>
2026-05-28 22:49:55 +0800

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -64,6 +64,7 @@ function Skills() {
               <span>Framer</span>
               <span>Figma</span>
               <span>CapCut</span>
+              <span>Filmora</span>
             </div>
           </div>
         </div>
